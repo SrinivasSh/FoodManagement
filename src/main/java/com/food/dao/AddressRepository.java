0a3675b84672d549272213d5686c8ee3400baec5
SrinivasSh <srinivasshanmukh999@gmail.com>
2025-12-26 01:12:40 +0530

    Remove root-level Restaurant service files

--- src/main/java/com/food/dao/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.food.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.food.Model.Address;
-
-public interface AddressRepository extends JpaRepository<Address, Integer>{
-
-}
--- src/main/java/com/food/dao/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.food.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.food.Model.Item;
-
-public interface ItemRepository extends JpaRepository<Item, Integer> {
-
-}
